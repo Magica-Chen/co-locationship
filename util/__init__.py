b39704803627ac32c3e836acbcbe7b5dc9b4e62f
Magica_Chen <sxtpy2010@gmail.com>
2020-11-28 21:45:27 +0000

    rename util to utils

--- util/__init__.py
+++ util/__init__.py
@@ -2,8 +2,8 @@ from .entropy import shannon_entropy, LZ_entropy
 from .cross_entropy import LZ_cross_entropy
 from .predictability import getPredictability
 from .preprocessing import pre_processing
-from .util import fast_indices
-from .util import tuple_concat
+from .utils import fast_indices
+from .utils import tuple_concat
 from .metric import jaccard_similarity
 from .metric import colocation_rate
 from .cumulative_cross_entropy import cumulative_LZ_CE
