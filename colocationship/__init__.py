25c1efcdfb9d451315fe4a3703fa63e8f22a8c2c
Exeter-server Zexun <sxtpy2010@gmail.com>
2021-12-13 16:04:50 +0000

    add file in init

--- colocationship/__init__.py
+++ colocationship/__init__.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # Class for co-locationship, meetup_strategy
-# (c) Zexun Chen, 2020-12-16
+# (c) Zexun Chen, 2021-12-13
 # sxtpy2010@gmail.com
 
 from . import util
 from .meetup_strategy import *
 from .meetup_comparison import *
+from .meetup_fast import *
